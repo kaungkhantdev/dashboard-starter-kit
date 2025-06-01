046cf17a9b3374d725dc0d84f006ac81e2a91104
kaungkhantcodigo <kaungkhantzaw@codigo.co>
2025-06-01 22:56:42 +0700

    add mobile responsive

--- src/features/setting/components/AccountContent.tsx
+++ src/features/setting/components/AccountContent.tsx
@@ -7,15 +7,15 @@ const AccountContent = () => {
   return (
     <div className="w-full">
         <div className="pb-2 border-b border-gray-100 dark:border-gray-900">
-            <h2 className="">Account</h2>
+            <h2 className="">Basics</h2>
         </div>
         
         {/* Photo */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Photo</span>
             </div>
-            <div className="col-span-3">
+            <div className="col-span-2 lg:col-span-3">
                 <img 
                     src="https://github.com/shadcn.png"
                     alt="Profile" 
@@ -29,10 +29,10 @@ const AccountContent = () => {
         
         {/* Name */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Name</span>
             </div>
-            <div className="col-span-3">
+            <div className="col-span-2 lg:col-span-3">
                 <span className="text-sm">Sophie Chamberlain</span>
             </div>
             <div className="col-span-1 text-end">
@@ -42,11 +42,11 @@ const AccountContent = () => {
 
         {/* Email Address */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Email Address</span>
             </div>
-            <div className="col-span-3">
-                <span className="text-sm">hi@sophiehamberlain.com</span>
+            <div className="col-span-2 lg:col-span-3 overflow-hidden">
+                <span className="text-sm text-wrap">hi@sophiehamberlain.com</span>
             </div>
             <div className="col-span-1 text-end">
                 <Button variant={'outline'} size={'sm'}>Edit</Button>
@@ -55,23 +55,21 @@ const AccountContent = () => {
 
         {/* Linked team account */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Linked team account</span>
                 <br />
                 <span className="text-sm text-muted-foreground">
-                    Easily switch between them and access <br /> both accounts from any device.
+                    Easily switch between <br className="lg:hidden" /> them and access <br /> both accounts from any device.
                 </span>
             </div>
-            <div className="col-span-3">
-               <div className="col-span-3">
-                    <img 
-                        src="https://github.com/shadcn.png"
-                        alt="Profile" 
-                        className="w-11 h-11 rounded-full object-cover"
-                    />
-                </div>
+            <div className="col-span-1 lg:col-span-3">
+               <img 
+                    src="https://github.com/shadcn.png"
+                    alt="Profile" 
+                    className="w-11 h-11 rounded-full object-cover"
+                />
             </div>
-            <div className="col-span-1 text-end">
+            <div className="col-span-2 lg:col-span-1 text-end">
                 <Button variant={'outline'} size={'sm'}>Manage Team</Button>
             </div>
         </div>
@@ -82,10 +80,10 @@ const AccountContent = () => {
         
         {/* Automatic */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Automatic Time</span>
             </div>
-            <div className="col-span-3">
+            <div className="col-span-2 lg:col-span-3">
                 <div className="flex items-center space-x-2">
                     <Switch id="airplane-mode" />
                     <Label htmlFor="airplane-mode">GMT +07:00</Label>
@@ -95,10 +93,14 @@ const AccountContent = () => {
 
         {/* Language */}
         <div className="grid grid-cols-6 border-b border-gray-100 dark:border-gray-900 w-full py-5">
-            <div className="col-span-2">
+            <div className="col-span-3 lg:col-span-2">
                 <span className="text-sm font-medium">Language</span>
+                <br />
+                <span className="text-sm text-muted-foreground">
+                   Default language for dashboard
+                </span>
             </div>
-            <div className="col-span-3">
+            <div className="col-span-2 lg:col-span-3">
                 <Select defaultValue="english_uk">
                     <SelectTrigger className="w-[180px]">
                         <SelectValue placeholder="Language" />
